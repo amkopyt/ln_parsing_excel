e96988b89b6728288e322a93026b236bcf9423c9
amkopyt <quzible@yandex.ru>
2022-03-25 18:14:57 +0300

    Initial commit

---
+++ resources/js/Main.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <div class="tet">
+        1231
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Main"
+}
+</script>
+
+<style scoped lang="scss">
+.tet {
+    margin-left: 20px;
+}
+</style>
