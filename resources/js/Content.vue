3338aadb7cede15ec723dfb24486cb975d6f7c21
amkopyt <quzible@yandex.ru>
2022-03-26 19:56:11 +0300

    feat(layout) add ContentTable

--- resources/js/Content.vue
+++ resources/js/Content.vue
@@ -1,20 +1,77 @@
 <template>
   <div class="content">
-    <h2 class="content__title">
-      Dashboard
-    </h2>
-    d
+    <div class="content__header">
+      <h2 class="content__title">
+        Dashboard
+      </h2>
+      <div class="content__date-filter">
+        <span>Aug 21, 2021 - Sep 21, 2021</span>
+        <img
+          src="@/assets/icons/fold.svg"
+          alt="fold/unfold"
+        >
+      </div>
+      <div class="content__print">
+        <img
+          src="@/assets/icons/print.svg"
+          alt="Print"
+        >
+      </div>
+      <div class="content__download">
+        <img
+          src="@/assets/icons/download.svg"
+          alt="Download"
+        >
+      </div>
+    </div>
+    <ContentTable />
   </div>
 </template>
 
 <script>
+import ContentTable from "@/components/ContentTable";
 export default {
-    name: "Content"
+    name: "Content",
+    components: {ContentTable}
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .content {
     background-color: #E2F0F0;
+}
+.content__header {
+    display: flex;
+    max-width: 920px;
+    align-items: center;
+}
+.content__header>*:nth-child(2) {
+    margin-left: auto;
+}
+.content__title {
+    font-family: 'Quicksand', sans-serif;
+    font-size: 34px;
+    margin: 12px 0;
+}
+.content__date-filter {
+    font-weight: 400;
+    font-size: 14px;
+    border: 1px solid #C6CACC;
+    border-radius: 10px;
+    padding: 7px 12px;
+    margin-top: 21px;
+
+    display: flex;
+    align-items: center;
+    img {
+        margin-left: 20px;
+    }
+}
+.content__print, .content__download {
+    margin-top: 21px;
+    margin-left: 8px;
+    background-color: #F7F9FA;
+    padding: 6.5px 10px;
+    border-radius: 4px;
 }
 </style>
---
+++ resources/js/components/ContentTable.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="ct-table">
+    <table>
+      <thead>
+        <tr class="ct-header">
+          <th
+            class="ct-header__data"
+          >
+            Data
+          </th>
+          <template
+            v-for="company in headers.companies"
+            class="ct-header__company"
+          >
+            <th
+              :key="company + '_qOil_fact'"
+              class="ct-header__values"
+            >
+              {{ company }} qOil fact
+            </th>
+            <th
+              :key="company + '_qOil_forecast'"
+              class="ct-header__values"
+            >
+              {{ company }} qOil forecast
+            </th>
+            <th
+              :key="company + '_qLiq_fact'"
+              class="ct-header__values"
+            >
+              {{ company }} qLiq fact
+            </th>
+            <th
+              :key="company + '_qLiq_forecast'"
+              class="ct-header__values"
+            >
+              {{ company }} qLiq forecast
+            </th>
+          </template>
+        </tr>
+      </thead>
+      <tbody class="ct-body">
+        <tr
+          v-for="item in data"
+          :key="item.date"
+        >
+          <td class="ct-body__date">
+            {{ item.date }}
+          </td>
+          <template v-for="company in item.companies">
+            <td
+              :key="company.name + '_qOil_fact'"
+              class="ct-body__values"
+            >
+              {{ company.qOil.fact }}
+            </td>
+            <td
+              :key="company.name + '_qOil_forecast'"
+              class="ct-body__values"
+            >
+              {{ company.qOil.forecast }}
+            </td>
+            <td
+              :key="company.name + '_qLiq_fact'"
+              class="ct-body__values"
+            >
+              {{ company.qLiq.fact }}
+            </td>
+            <td
+              :key="company.name + '_qLiq_forecast'"
+              class="ct-body__values"
+            >
+              {{ company.qLiq.forecast }}
+            </td>
+          </template>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ContentTable",
+    data() {
+        return {
+            headers: {
+              companies: ['company1', 'company2']
+            },
+            data: [
+                {
+                    date: 'Data1',
+                    companies: [
+                        {
+                            name: 'company1',
+                            qOil: {
+                                fact: 2,
+                                forecast: 1
+                            },
+                            qLiq: {
+                                fact: 4,
+                                forecast: 1
+                            }
+                        },
+                        {
+                            name: 'company2',
+                            qOil: {
+                                fact: 2,
+                                forecast: 1
+                            },
+                            qLiq: {
+                                fact: 4,
+                                forecast: 1
+                            }
+                        },
+                    ]
+                },
+                {
+                    date: 'Data2',
+                    companies: [
+                        {
+                            name: 'company1',
+                            qOil: {
+                                fact: 4,
+                                forecast: 33
+                            },
+                            qLiq: {
+                                fact: 4,
+                                forecast: 1
+                            }
+                        },
+                        {
+                            name: 'company2',
+                            qOil: {
+                                fact: 2,
+                                forecast: 23
+                            },
+                            qLiq: {
+                                fact: 4,
+                                forecast: 1
+                            }
+                        },
+                    ]
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.ct-table {
+    max-width: 920px;
+    overflow-x: scroll;
+}
+.ct-header {
+    font-family: 'Quicksand', sans-serif;
+    font-weight: 500;
+    letter-spacing: 0.44px;
+    color: #919699;
+    border-bottom: 2px solid #E3E5E6;
+}
+.ct-header__data {
+    text-align: left;
+    min-width: 330px;
+    border-bottom: 2px solid #E3E5E6;
+}
+.ct-header__values {
+    min-width: 118px;
+    padding: 5px;
+    border-bottom: 2px solid #E3E5E6;
+}
+.ct-body {
+    font-weight: 500;
+    font-size: 18px;
+    letter-spacing: 0.15px;
+}
+.ct-body__date {
+    border-bottom: 1px solid #E3E5E6;
+}
+.ct-body__values {
+    border-bottom: 1px solid #E3E5E6;
+    padding: 18px 10px;
+    text-align: right;
+    color: #5E6366;
+}
+</style>
+
