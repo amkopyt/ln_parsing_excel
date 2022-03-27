d9d67d57f19881a2ee4822efb7d1e1b1be69b715
amkopyt <quzible@yandex.ru>
2022-03-27 17:38:51 +0300

    feat(routes) add 2 routes for import and get data

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('data', [\App\Http\Controllers\DataController::class, 'getData']);
+Route::post('data/upload-excel', [\App\Http\Controllers\DataController::class, 'uploadExcel']);
